d8875c558ecbf087209801d914690fa589c824bd
Nic Young <nryoung@gmail.com>
2017-02-26 10:46:49 -0700

    feat: add area func. for right sided triangles

--- week1/second.erl
+++ week1/second.erl
@@ -2,8 +2,12 @@
 
 -import(first, [square/1]).
 
--export([hypotenuse/2]).
+-export([hypotenuse/2,area/2]).
 
 hypotenuse(A,B) ->
 
     math:sqrt(square(A)+square(B)).
+
+area(A,B) ->
+
+    (A*B)/2.
