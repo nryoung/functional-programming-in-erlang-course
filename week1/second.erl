da1db6096bf50ce30d1326a046f928e03f0c41db
Nic Young <nryoung@gmail.com>
2017-02-26 10:56:45 -0700

    feat: add perimeter func.

--- week1/second.erl
+++ week1/second.erl
@@ -2,7 +2,7 @@
 
 -import(first, [square/1]).
 
--export([hypotenuse/2,area/2]).
+-export([hypotenuse/2,area/2,perimeter/2]).
 
 hypotenuse(A,B) ->
 
@@ -11,3 +11,7 @@ hypotenuse(A,B) ->
 area(A,B) ->
 
     (A*B)/2.
+
+perimeter(A,B) ->
+
+    A+B+math:sqrt(square(A)+square(B)).
