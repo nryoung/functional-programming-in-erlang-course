e8bb21e481af10ef9541e62381df80836a4654aa
Nic Young <nryoung@gmail.com>
2017-02-26 22:15:56 -0700

    feat: add howManyEqual func

---
+++ week1/howManyEqual.erl
@@ -0,0 +1,18 @@
+-module(howManyEqual).
+
+-export([howManyEqual/3]).
+
+howManyEqual(X,X,X) ->
+    3;
+
+howManyEqual(X,X,_) ->
+    2;
+
+howManyEqual(X,_,X) ->
+    2;
+
+howManyEqual(_,X,X) ->
+    2;
+
+howManyEqual(_,_,_) ->
+    0.
